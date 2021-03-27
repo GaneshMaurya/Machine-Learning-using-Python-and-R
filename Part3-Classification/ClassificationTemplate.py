755c2555ec29ff9742aa28348c111903d84ed53d
Ganesh Maurya <gvgmaurya@gmail.com>
2021-03-27 22:40:23 +0530

    classifier template update

--- Part3-Classification/ClassificationTemplate.py
+++ Part3-Classification/ClassificationTemplate.py
@@ -36,7 +36,7 @@ print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),
 #Making the confusion matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
-accuracy_score(y_test, y_pred)
+print(accuracy_score(y_test, y_pred))
 
 
 #Visualizing the Training set results
