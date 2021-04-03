b6b6ad9c467fda7524c5678656eba08d5202b756
Ganesh Maurya <gvgmaurya@gmail.com>
2021-04-03 13:04:22 +0530

    eclat

--- Part5-Association-Rule-Learning/Section 29 - Eclat/Python/eclat.py
+++ Part5-Association-Rule-Learning/Section 29 - Eclat/Python/eclat.py
@@ -1,8 +1,9 @@
-# Apriori
+# Eclat
 
-
+'''
 # Install Apyori
 !pip install apyori
+'''
 
 
 # Import the Libraries
@@ -20,7 +21,7 @@ for i in range(0, 7501):
     transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
     
 
-# Training the Apriori Model on the dataset
+# Training the Eclat Model on the dataset
 
 from apyori import apriori
 rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
@@ -37,13 +38,8 @@ def inspect(results):
     lhs         = [tuple(result[2][0][0])[0] for result in results]
     rhs         = [tuple(result[2][0][1])[0] for result in results]
     supports    = [result[1] for result in results]
-    confidences = [result[2][0][2] for result in results]
-    lifts       = [result[2][0][3] for result in results]
-    return list(zip(lhs, rhs, supports, confidences, lifts))
-resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
-
-# Displaying the results non sorted
-resultsinDataFrame
+    return list(zip(lhs, rhs, supports))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
 
 # Displaying the results sorted by descending lifts
-resultsinDataFrame.nlargest(n = 10, columns = 'Lift')
+resultsinDataFrame.nlargest(n = 10, columns = 'Support')
